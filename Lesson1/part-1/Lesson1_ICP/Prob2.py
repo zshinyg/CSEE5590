7e33b84256644db2e1c0cd30905e9e33ef2303af
Coltyn Gatton <c527g802@ku.edu>
2020-06-09 12:47:28 -0500

    Prob1
    Finsished Prob1

---
+++ Lesson1/part-1/Lesson1_ICP/Prob2.py
@@ -0,0 +1,20 @@
+# PROB 2:  Input the string “Python” as a list of characters from console, 
+#          delete at least 2 characters, reverse the resultant string and print it.
+
+import random
+
+PythonStr = "str"
+
+while(PythonStr != 'Python'):                                   # Take in Python input from console
+    PythonStr = input("Please type the string 'Python':") 
+
+pyList = list(PythonStr)                                        # convert to list of chars
+
+pyList.remove('P')                                              # remove H and O
+pyList.remove('o')
+
+pyList.reverse()                                                # Reverse the List
+
+newPythonStr = ''.join(pyList)                                  # Convert List to String
+
+print(newPythonStr)
