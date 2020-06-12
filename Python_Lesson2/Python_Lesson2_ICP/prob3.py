f75ef0ea3abbc7f99f8d9a65475e17111f0fa920
Coltyn Gatton <c527g802@ku.edu>
2020-06-11 21:21:15 -0500

    Finished Prob3

--- Python_Lesson2/Python_Lesson2_ICP/prob3.py
+++ Python_Lesson2/Python_Lesson2_ICP/prob3.py
@@ -1,12 +1,22 @@
 dict_wordCount = dict()
 
-with open("prob3.txt", 'w+') as infile:
-    for line in infile:
+with open('prob3.txt', 'r') as file:
+    for line in file:
         for word in line.split():
             if word in dict_wordCount:
                 dict_wordCount[word] = dict_wordCount[word] + 1
             if not word in dict_wordCount:
                 dict_wordCount[word] = 1
+file.close()
 
-print(dict_wordCount)
+file = open('prob3.txt', 'w')
+for x in dict_wordCount: 
+    print(x,":", dict_wordCount[x])
+    file.write(x)
+    file.write(": ")
+    file.write(str(dict_wordCount[x]))
+    file.write("\n")
+
+    
+file.close
         
