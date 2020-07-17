b56659a0618fe9b59cbbdbddc42a469b8761505f
Coltyn Gatton <c527g802@ku.edu>
2020-07-16 22:25:59 -0500

    ICP4

---
+++ DeepLearning_Lesson3/sentiment_analysis.py
@@ -0,0 +1,30 @@
+from keras.models import Sequential
+from keras import layers
+from keras.preprocessing.text import Tokenizer
+import pandas as pd
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv('imdb_master.csv',encoding='latin-1')
+print(df.head())
+sentences = df['review'].values
+y = df['label'].values
+
+
+#tokenizing data
+tokenizer = Tokenizer(num_words=2000)
+tokenizer.fit_on_texts(sentences)
+#getting the vocabulary of data
+sentences = tokenizer.texts_to_matrix(sentences)
+
+le = preprocessing.LabelEncoder()
+y = le.fit_transform(y)
+X_train, X_test, y_train, y_test = train_test_split(sentences, y, test_size=0.25, random_state=1000)
+
+# Number of features
+# print(input_dim)
+model = Sequential()
+model.add(layers.Dense(300,input_dim=input_dim, activation='relu'))
+model.add(layers.Dense(5, activation='sigmoid'))
+model.compile(loss='sparse_categorical_crossentropy',optimizer='adam',metrics=['acc'])
+history=model.fit(X_train,y_train, epochs=5, verbose=True, validation_data=(X_test,y_test), batch_size=256)
